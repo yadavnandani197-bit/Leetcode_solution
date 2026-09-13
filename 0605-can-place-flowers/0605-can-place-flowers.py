31b75430d7a20ec5c4135ef6b286d7afb3a6ab4c
Nandani Kumari Yadav <yadavnandani197@gmail.com>
2026-09-14 00:43:53 +0530

    Time: 12 ms (41.63%), Space: 13 MB (50.42%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        count = 0
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 0:
+                left_empty = (i == 0) or (flowerbed[i-1] == 0)
+                right_empty = (i == len(flowerbed)-1) or (flowerbed[i+1] == 0)
+
+                if left_empty and right_empty:
+                    flowerbed[i] = 1 
+                    count += 1
+                    if count >= n:
+                        return True
+
+        return count >= n
